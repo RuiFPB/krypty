9fe190acac046e04b983f119a20c61b52fb6c2ab
Rui Barbosa <rpinhobarbosa@gmail.com>
2023-01-28 15:45:34 +0000

    Handling errors and support for decimals

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpc.h"
@@ -27,28 +28,126 @@ void add_history(char *unused) {}
 #include <editline/history.h>
 #endif
 
-// Use the operator string to see the operation to perform
-long eval_op(long x, char *op, long y) {
-    if(strcmp(op, "+") == 0) { return x + y; }
-    if(strcmp(op, "-") == 0) { return x - y; }
-    if(strcmp(op, "*") == 0) { return x * y; }
-    if(strcmp(op, "/") == 0) { return x / y; }
-    if(strcmp(op, "%") == 0) { return x % y; }
-    return 0;
+typedef struct {
+    int type;
+    union {
+        int error;
+        double num_dec;
+        long num;
+    };
+} lval;
+
+enum { LVAL_NUM, LVAL_NUM_DEC, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// Create a new number type lval
+lval lval_dec(double x) {
+    lval v;
+    v.type = LVAL_NUM_DEC;
+    v.num_dec = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
+// Create a new number type lval
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+// Create a new error type lval
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.error = x;
+    return v;
+}
+
+// Converts numbers to decimal lval 
+lval lval_num_to_dec(lval x) {
+    if (x.type == LVAL_NUM) {
+        x.type = LVAL_NUM_DEC;
+        x.num_dec = (double)x.num;
+    }
+    return x;
+}
+
+// Print the lval
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%ld", v.num); break;
+        case LVAL_NUM_DEC: printf("%lf", v.num_dec); break;
+        case LVAL_ERR:
+            if (v.error == LERR_DIV_ZERO) 
+                printf("Error: Divide by Zero!");
+            if (v.error == LERR_BAD_OP) 
+                printf("Error: Invalid Operator!");
+            if (v.error == LERR_BAD_NUM)
+                printf("Error: Invalid Number!");
+            break;
+    }
+}
+
+// Print the lval and crete a new line
+void lval_println(lval v) { lval_print(v); putchar('\n'); };
+
+// Use the operator string to see the operation to perform
+lval eval_op(lval x, char *op, lval y) {
+#ifdef DEBUG
+    printf("x: "); lval_println(x);
+    printf("y: "); lval_println(y);
+#endif
+    if(x.type == LVAL_ERR) { return x; }
+    if(y.type == LVAL_ERR) { return y; }
+    if(x.type == LVAL_NUM && y.type == LVAL_NUM) {
+        if(strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+        if(strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+        if(strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+        if(strcmp(op, "/") == 0) { 
+            return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+        }
+        if(strcmp(op, "%") == 0) {
+            return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+        }
+    } else {
+        x = lval_num_to_dec(x);
+        y = lval_num_to_dec(y);
+        if(strcmp(op, "+") == 0) { return lval_dec(x.num_dec + y.num_dec); }
+        if(strcmp(op, "-") == 0) { return lval_dec(x.num_dec - y.num_dec); }
+        if(strcmp(op, "*") == 0) { return lval_dec(x.num_dec * y.num_dec); }
+        if(strcmp(op, "/") == 0) { 
+            return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_dec(x.num_dec / y.num_dec);
+        }
+    }
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
 
     // if is number, return
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+        long x = strtol(t-> contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
+    if (strstr(t->tag, "decimal")) {
+        errno = 0;
+#ifdef DEBUG
+        printf("decimal: %s\n", t->contents);
+#endif
+        double x = strtod(t->contents, NULL);
+#ifdef DEBUG
+        printf("val: %lf\n", x);
+#endif
+        return errno != ERANGE ? lval_dec(x) : lval_err(LERR_BAD_NUM);
     }
 
     // operator is always the third child
     char *op = t->children[1]->contents;
 
     // Store the second child in x
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     // Iterate the remaining children
     if (t->children_num > 4) {
@@ -58,17 +157,16 @@ long eval(mpc_ast_t* t) {
             i++;
         }
     } else {
-        if (strcmp(op, "-") == 0) {
-            x = -x;
-        }
+        if (strcmp(op, "-") == 0 && x.type == LVAL_NUM) x.num = -x.num;
+        if (strcmp(op, "-") == 0 && x.type == LVAL_NUM_DEC) x.num_dec = -x.num_dec;
     }
-
     return x;
 }
 
 int main(int argc, char **argv) {
     // Create the parsers
     mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Decimal   = mpc_new("decimal");
     mpc_parser_t* Operator  = mpc_new("operator");
     mpc_parser_t* Expr      = mpc_new("expr");
     mpc_parser_t* Krypty    = mpc_new("krypty");
@@ -76,13 +174,14 @@ int main(int argc, char **argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
             "                                                           \
                 number   : /-?[0-9]+/ ;                                 \
+                decimal  : /-?[0-9]+\\.[0-9]+/ ;                       \
                 operator : '+' | '-' | '*' | '/' | '%' ;                \
-                expr     : <number> | '(' <operator> <expr>+ ')' ;      \
+                expr     : <decimal> | <number> | '(' <operator> <expr>+ ')' ;      \
                 krypty   : /^/ <operator> <expr>+ /$/ ;                 \
             ", 
-            Number, Operator, Expr, Krypty);
+            Number, Decimal, Operator, Expr, Krypty);
 
-    puts("krypty Version 0.0.3");
+    puts("Krypty Version 0.0.4");
     puts("Developed by RuiFPB");
     puts("Check https://www.buildyourownlisp.com to build your own language");
     puts("Press Ctrl+C to Exit\n");
@@ -97,12 +196,12 @@ int main(int argc, char **argv) {
         // (Attempt to) Parse the user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Krypty, &r)) {
-
-            long result = eval(r.output);
-            printf("%ld\n", result);
+            
 #ifdef DEBUG
             mpc_ast_print(r.output);
 #endif
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
 
         } else {
